c42870874a127526e9b2bca5bd2655cd0fbe551b
narutompel <putujackson0@gmail.com>
2024-06-05 18:55:37 +0800

    Menghapus variabel width di class _CoursesDetailState

--- lib/courses_detail.dart
+++ lib/courses_detail.dart
@@ -13,7 +13,6 @@ class _CoursesDetailState extends State<CoursesDetail> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: SingleChildScrollView(
